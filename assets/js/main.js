0062fa4b512420256d1b28331fc76dbcedf3539e
Artem Sheludko <sheludko.artem@gmail.com>
2017-09-06 14:52:19 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,14 +19,4 @@ $(document).ready(function() {
   }
   });
 
-  SimpleJekyllSearch({
-    searchInput: document.getElementById('search-input'),
-    resultsContainer: document.getElementById('results-container'),
-    json: '{{site.baseurl}}/search.json',
-    searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
-    noResultsText: 'No results found',
-    fuzzy: false,
-    exclude: ['Welcome']
-  });
-
 });
