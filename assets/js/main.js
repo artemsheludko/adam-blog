9bd57051663fe87342242a86fc02f5bc3284583e
artemsheludko <sheludko.artem@gmail.com>
2017-09-06 13:28:40 +0300

    init project

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+  // Menu Settings
+  $('.menu-icon, .menu-icon-close').click(function(e) {
+    e.preventDefault();
+    $('.flex-container').toggleClass('active');
+  });
+
+  // Search Settings
+  $('.search-icon').on('click', function(e){
+    e.preventDefault();
+    $('.search-box').toggleClass('search-active');
+
+    if ($('.search-box').hasClass('search-active')) {
+      $('.search-icon-close').on('click', function(e){
+  		e.preventDefault();
+  		$('.search-box').removeClass('search-active');
+  	});
+  }
+  });
+
+  SimpleJekyllSearch({
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    json: '/search.json',
+    searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
+    noResultsText: 'No results found',
+    fuzzy: false,
+    exclude: ['Welcome']
+  });
+
+});
